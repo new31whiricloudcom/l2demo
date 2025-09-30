c4964db64c85051ed0136c3ccc4f3200528caeec
Klementaun <clemens.wag@icloud.com>
2025-09-30 15:13:01 +0200

    Initial Commit

---
+++ l2demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  l2demo
+//
+//  Created by Clemens Wagner on 30.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ l2demo/l2demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  l2demoApp.swift
+//  l2demo
+//
+//  Created by Clemens Wagner on 30.09.25.
+//
+
+import SwiftUI
+
+@main
+struct l2demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
