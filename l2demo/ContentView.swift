1fa95b299f0a9e0108a141bd2708ac8cbdcfbd66
Klementaun <clemens.wag@icloud.com>
2025-09-30 17:16:55 +0200

    testing

--- l2demo/ContentView.swift
+++ l2demo/ContentView.swift
@@ -9,30 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("was ist fußball")
-                .font(.largeTitle)
-            HStack {
-                Image(systemName: "figure.american.football")
+        ZStack{
+            Color(.yellow)
+                .ignoresSafeArea()
+            
+            VStack (alignment: .leading, spacing: 20){
+                
+                
+                Image("busride")
                     .resizable()
                     .scaledToFit()
-
-                Image(systemName: "figure.australian.football")
-                    .resizable()
-                    .scaledToFit()
-
-                Image(systemName: "figure.indoor.soccer")
-                    .resizable()
-                    .scaledToFit()
-
-
-
+                    .cornerRadius(20)
+                HStack{
+                    
+                    Text("japans rual area")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                            Image(systemName: "star")
+                        }                          .foregroundStyle(.orange)
+                            .font(.caption)
+                        
+                        
+                        Text("(284 reviews)")
+                            .font(.caption)
+                            .foregroundStyle(.blue)
+                        
+                    }
+                    
+                    
+                }
+                Text("This is what you expirence when you are on a long bus ride in japan.")
+                HStack{
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }.foregroundStyle(.gray)
+                    .font(.caption)
+                
             }
-            .foregroundStyle(.cyan)
+            .padding()
+            .background(Rectangle().foregroundStyle(.white)
+                .cornerRadius(20)
+                .shadow(radius: 15)
+            )
+            .padding()
         }
-        .padding()
+     }
+        
     }
-}
+
 
 #Preview {
     ContentView()
